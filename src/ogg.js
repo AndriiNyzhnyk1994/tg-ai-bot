6003975c9c3a9ce94ae5b25d39eabf41eaacb566
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-12-19 00:40:52 +0200

    added logic to working with mp3

--- src/ogg.js
+++ src/ogg.js
@@ -2,14 +2,26 @@ import axios from "axios"
 import { createWriteStream } from "fs"
 import { dirname, resolve } from "path"
 import { fileURLToPath } from "url"
+import ffmpeg from "fluent-ffmpeg"
+import installer from '@ffmpeg-installer/ffmpeg'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 class OggConverter {
-    constructor() { }
+    constructor() {
+        ffmpeg.setFfmpegPath(installer.path)
+     }
     toMp3(input, output) {
         try {
-            const outputPath = resolve(dirname(input))
+            const outputPath = resolve(dirname(input), `${output}.mp3`)
+            return new Promise((resolve, reject) => {
+                ffmpeg(input)
+                .inputOption('-t 30')
+                .output(outputPath)
+                .on('end', () => resolve(outputPath))
+                .on('error', err => reject(err.message))
+                .run()
+            })
         } catch (error) {
             console.log('Error while creating mp3', error.message);
         }
