2ae9c2a41f502869256b801dabef69f187efbbac
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-12-19 22:38:30 +0200

    added transcription^ voise to text

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { Telegraf } from "telegraf"
 import config from 'config'
 import { message } from 'telegraf/filters'
 import { ogg } from './ogg.js' 
+import { openai } from './openai.js' 
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
@@ -10,10 +11,16 @@ bot.on(message('voice'), async ctx => {
     try {
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
         const userId = String(ctx.message.from.id)
+        const date = String(ctx.message.date)
         console.log(link.href)
         const oggPath = await ogg.create(link.href, userId)
         const mp3Path = await ogg.toMp3(oggPath, userId)
-        await ctx.reply(mp3Path)
+        
+        const text = await openai.transcription(mp3Path)
+        // const response = await openai.chat(text)
+        
+        await ctx.reply(text ? text : 'nothing recorded')
+        console.log(ctx.message);
     } catch (e) {
         console.log('Error while voice message', e.message);
     }
